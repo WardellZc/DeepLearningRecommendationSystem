bd7a31584168996a1aca820232f71a8409680eff
WardellZc <164826869+WardellZc@users.noreply.github.com>
2024-04-20 08:42:08 +0800

    Update lr.py

--- model/lr.py
+++ model/lr.py
@@ -4,20 +4,24 @@
 # @File    : mf.py
 import torch
 from torch import nn
-from torch.nn.init import xavier_normal_
 
 
 class LogisticRegression(nn.Module):
-    def __init__(self, num_feature: int, num_constant: int == 1):
+    def __init__(self, num_feature: int):
         super().__init__()
+        # 特征向量参数w和b
+        self.linear = nn.Linear(num_feature, 1, True)
 
-        # 用户和物品的embedding
-        self.W = nn.Embedding(num_feature, 1)
-        self.B = nn.Embedding(num_constant, 1)
+    def forward(self, feature_vector: torch.Tensor) -> torch.Tensor:
+        return torch.sigmoid(self.linear(feature_vector))
 
-        # embedding初始化
-        xavier_normal_(self.W.weight.data)
-        xavier_normal_(self.B.weight.data)
-
-    def forward(self, feature_vactor: torch.Tensor) -> torch.Tensor:
-        return torch.sigmoid(torch.matmul(feature_vactor, self.W.weight) + self.B.weight)
+    def recommendation(self, num_users, user_item, k):
+        array = []
+        for i in range(num_users):
+            user_vector = user_item[user_item['user_id'] == i]
+            user_vector = torch.Tensor(user_vector.iloc[:, 2:].values)
+            scores = self.forward(user_vector)
+            values, indices = torch.topk(scores, k, dim=0)
+            indices = indices.view(1, -1).tolist()[0]
+            array.append(indices)
+        return array
