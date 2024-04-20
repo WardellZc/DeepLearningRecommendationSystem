3116052ce2ea955781c5baa3f59445ed9ea08833
WardellZc <164826869+WardellZc@users.noreply.github.com>
2024-04-20 08:44:40 +0800

    Update ffm.py

--- scripts/ffm.py
+++ scripts/ffm.py
@@ -15,81 +15,78 @@ from sampler.sampler import Sampler
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 data = MovieLens100K('../dataset_example/ml-100k')
-train_data, test_data, user_item = data.feature_data('../dataset_example/ml-100k')
 
-train_userid_itemid = train_data[:, :2]
-test_userid_itemid = test_data[:, :2]
-train_excluded_pairs = set(tuple(pair.tolist()) for pair in train_userid_itemid)
-test_excluded_pairs = set(tuple(pair.tolist()) for pair in test_userid_itemid)
-
-# 训练集负样本采集
+# 负采样
+# 将训练集、验证集、测试集中存在的用户-项目对得出
+train_set = set(data.train.apply(lambda row: (row['user_id'], row['item_id']), axis=1))
+validation_set = set(data.validation.apply(lambda row: (row['user_id'], row['item_id']), axis=1))
+test_set = set(data.test.apply(lambda row: (row['user_id'], row['item_id']), axis=1))
+excluded_pairs = train_set | validation_set | test_set
+# 生成训练集负样本，并合并数据
 train_sampler = Sampler()
-train_negative_users, train_negative_items = train_sampler.rns(data.num_users, data.num_items, train_excluded_pairs, 10)
-train_negative_rating = [0 for _ in range(len(train_negative_users))]
-train_negative_data = {'user_id': train_negative_users, 'item_id': train_negative_items,
-                       'rating': train_negative_rating}
-train_negative_data = pd.DataFrame(train_negative_data)
-train_negative_data = pd.merge(train_negative_data, data.item, on='item_id').drop('key', axis=1)
-train_negative_data = pd.merge(train_negative_data, data.user, on='user_id').drop('key', axis=1)
-train_negative_data = torch.tensor(train_negative_data.values, dtype=torch.float32)
-train_data = torch.cat((train_data, train_negative_data), dim=0)
-
-# 测试集负样本采集
+train_negative = train_sampler.negative_sampling2(data.num_users, data.num_items, excluded_pairs, 10)  # 生成用户、项目、评分数据
+train_negative = pd.merge(train_negative, data.user_data, on='user_id')  # 将负样本也连接上用户和物品信息
+train_negative = pd.merge(train_negative, data.item_data, on='item_id')
+train_combined = pd.concat([data.train, train_negative], axis=0).reset_index(drop=True)  # 合并正负样本
+# 生成验证集负样本，并合并数据
+validation_sampler = Sampler()
+validation_negative = validation_sampler.negative_sampling2(data.num_users, data.num_items, excluded_pairs, 10)
+validation_negative = pd.merge(validation_negative, data.user_data, on='user_id')
+validation_negative = pd.merge(validation_negative, data.item_data, on='item_id')
+validation_combined = pd.concat([data.validation, validation_negative], axis=0).reset_index(drop=True)
+# 生成测试集负样本，并合并数据
 test_sampler = Sampler()
-test_negative_users, test_negative_items = test_sampler.rns(data.num_users, data.num_items, test_excluded_pairs, 2)
-test_negative_rating = [0 for _ in range(len(test_negative_users))]
-test_negative_data = {'user_id': test_negative_users, 'item_id': test_negative_items, 'rating': test_negative_rating}
-test_negative_data = pd.DataFrame(test_negative_data)
-test_negative_data = pd.merge(test_negative_data, data.item, on='item_id').drop('key', axis=1)
-test_negative_data = pd.merge(test_negative_data, data.user, on='user_id').drop('key', axis=1)
-test_negative_data = torch.tensor(test_negative_data.values, dtype=torch.float32)
-test_data = torch.cat((test_data, test_negative_data), dim=0)
+test_negative = test_sampler.negative_sampling2(data.num_users, data.num_items, excluded_pairs, 10)
+test_negative = pd.merge(test_negative, data.user_data, on='user_id')
+test_negative = pd.merge(test_negative, data.item_data, on='item_id')
+test_combined = pd.concat([data.test, test_negative], axis=0).reset_index(drop=True)
 
-# 筛选训练特征（电影特征、年龄、性别、职业）
-train_movie_age = train_data[:, 3:23]
-list1 = list(range(3, 22)) + [23]
-train_movie_gender = train_data[:, list1]
-list2 = list(range(3, 22)) + [24]
-train_movie_occupation = train_data[:, list2]
-train_age_gender = train_data[:, 22:24]
-list3 = [22, 24]
-train_age_occupation = train_data[:, list3]
-train_gender_occupation = train_data[:, 23:25]
-train_real = train_data[:, 2]
-train_real = train_real.view(-1, 1)  # 转化为二维张量
+# 生成训练集、验证集、测试集的训练特征向量以及评分的torch
+train_data = torch.tensor(train_combined.iloc[:, 3:].values, dtype=torch.float32)  # 去除user_di、item_id、rating
+train_rating = torch.tensor(train_combined.iloc[:, 2].values, dtype=torch.float32).unsqueeze(1)  # 评分数据，用于计算损失
+validation_data = torch.tensor(validation_combined.iloc[:, 3:].values, dtype=torch.float32)
+validation_rating = torch.tensor(validation_combined.iloc[:, 2].values, dtype=torch.float32).unsqueeze(1)
+test_data = torch.tensor(test_combined.iloc[:, 3:].values, dtype=torch.float32)
+test_rating = torch.tensor(test_combined.iloc[:, 2].values, dtype=torch.float32).unsqueeze(1)
 
-# 筛选测试特征
-test_movie_age = test_data[:, 3:23]
-list1 = list(range(3, 22)) + [23]
-test_movie_gender = test_data[:, list1]
-list2 = list(range(3, 22)) + [24]
-test_movie_occupation = test_data[:, list2]
-test_age_gender = test_data[:, 22:24]
-list3 = [22, 24]
-test_age_occupation = test_data[:, list3]
-test_gender_occupation = test_data[:, 23:25]
-test_real = test_data[:, 2]
-test_real = test_real.view(-1, 1)  # 转化为二维张量
-
-model = FFM(10)
+model = FFM(train_data.shape[1], 64)
 loss_fn = torch.nn.BCELoss()
-optimizer = optim.Adam(model.parameters(), lr=0.01)
+optimizer = optim.Adam(model.parameters(), lr=0.001)
 
+# 模型训练
 epochs = 50
 for epoch in range(epochs):
     model.train()
     optimizer.zero_grad()
-    predictions = model(train_movie_age, train_movie_gender, train_movie_occupation, train_age_gender,
-                        train_age_occupation, train_gender_occupation)
-    loss = loss_fn(predictions, train_real)
-    loss.backward()
+    train_predictions = model(train_data)
+    train_loss = loss_fn(train_predictions, train_rating)
+    train_loss.backward()
     optimizer.step()
 
     model.eval()
     with torch.no_grad():
-        predictions_test = model(test_movie_age, test_movie_gender, test_movie_occupation, test_age_gender,
-                                 test_age_occupation, test_gender_occupation)
-        test_loss = loss_fn(predictions_test, test_real)
-    print(f'Epoch {epoch + 1}: Training loss: {loss.item()} Test Loss: {test_loss.item()}')
+        validation_predictions = model(validation_data)
+        validation_loss = loss_fn(validation_predictions, validation_rating)
+        if epoch < epochs - 1:
+            print(f'Epoch {epoch + 1}: Training loss: {train_loss.item()} Validation Loss: {validation_loss.item()}')
+        else:
+            test_predictions = model(test_data)
+            test_loss = loss_fn(test_predictions, test_rating)
+            print(
+                f'Epoch {epoch + 1}: Training loss: {train_loss.item()} Validation Loss: {validation_loss.item()} Test Loss: {test_loss.item()}')
 
-
+# 推荐部分
+recommendation = model.recommendation(data.num_users, data.user_item, 100)
+same = 0.0
+p = 0.0
+r = 0.0
+for i in range(data.num_users):
+    real_list = data.data[data.data['user_id'] == i]['item_id'].values  # 用户真实评过分的物品id列表
+    recommendation_list = recommendation[i]  # 用户的推荐列表
+    same += len(set(real_list) & set(recommendation_list))
+    p += len(recommendation_list)  # 推荐总数
+    r += len(real_list)  # 用户真实评过分的物品数目
+precision = same / p
+recall = same / r
+f1 = 2*precision*recall/(precision+recall)
+print(f'Precision: {precision},Recall: {recall},F1: {f1}')
