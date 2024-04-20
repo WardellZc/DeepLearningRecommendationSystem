1994ea8c698c39466690fe15fd049ae7c8fb620c
WardellZc <164826869+WardellZc@users.noreply.github.com>
2024-04-20 08:45:18 +0800

    Update mf.py

--- scripts/mf.py
+++ scripts/mf.py
@@ -6,80 +6,93 @@
 import torch.nn
 from torch import optim
 
+import sys
+
+sys.path.append('../')
+
 from data.reader import MovieLens100K
 from model.mf import MatrixFactorization
-
-
-def negative_sampling(num_items: int,
-                      potential_negatives: torch.Tensor,
-                      excluded_pairs: set,
-                      num_negatives: int = 1,
-                      device: str = 'cpu'):
-    negative_items = []
-    negative_users = []
-
-    for user in potential_negatives:
-        user = user.item()
-        for _ in range(num_negatives):
-            negative_item = torch.randint(0, num_items, (1,)).item()
-            while (user, negative_item) in excluded_pairs:
-                negative_item = torch.randint(0, num_items, (1,)).item()
-            negative_items.append(negative_item)
-            negative_users.append(user)
-
-    return (torch.tensor(negative_users).to(device),
-            torch.tensor(negative_items).to(device),
-            torch.zeros(len(negative_users)).to(device))
-
+from sampler.sampler import Sampler
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 data = MovieLens100K('../dataset_example/ml-100k')
+
+# 读取数据
 train_user, train_item, train_rating = data.train_interaction(device=device)
+validation_user, validation_item, validation_rating = data.validation_interaction(device=device)
 test_user, test_item, test_rating = data.test_interaction(device=device)
+
 model = MatrixFactorization(data.num_users, data.num_items, 64).to(device)
 loss_fn = torch.nn.BCELoss()
 optimizer = optim.Adam(model.parameters(), lr=0.01)
 
-train_excluded_pairs = set(zip(test_user.cpu().numpy(), test_item.cpu().numpy()))
-test_excluded_pairs = set(zip(train_user.cpu().numpy(), train_item.cpu().numpy()))
+# 负采样
+# 先得到训练集、验证集、测试集中已有的用户-项目对
+excluded_pairs = set(zip(train_user.cpu().numpy(), train_item.cpu().numpy())) | set(
+    zip(validation_user.cpu().numpy(), validation_item.cpu().numpy())) | set(
+    zip(test_user.cpu().numpy(), test_item.cpu().numpy()))
+# 生成训练集负样本，并合并数据
+train_sampler = Sampler()
+train_negative_users, train_negative_items, train_negative_ratings = train_sampler.negative_sampling(data.num_users,
+                                                                                                     data.num_items,
+                                                                                                     excluded_pairs,
+                                                                                                     10)
+train_users_combined = torch.cat([train_user, train_negative_users]).to(device)
+train_items_combined = torch.cat([train_item, train_negative_items]).to(device)
+train_ratings_combined = torch.cat([train_rating, train_negative_ratings]).to(device)
+# 生成验证集负样本，并合并数据
+validation_sampler = Sampler()
+validation_negative_users, validation_negative_items, validation_negative_ratings = validation_sampler.negative_sampling(
+    data.num_users,
+    data.num_items,
+    excluded_pairs,
+    10)
+validation_users_combined = torch.cat([validation_user, validation_negative_users]).to(device)
+validation_items_combined = torch.cat([validation_item, validation_negative_items]).to(device)
+validation_ratings_combined = torch.cat([validation_rating, validation_negative_ratings]).to(device)
+# 生成测试集负样本，并合并数据
+test_sampler = Sampler()
+test_negative_users, test_negative_items, test_negative_ratings = test_sampler.negative_sampling(data.num_users,
+                                                                                                 data.num_items,
+                                                                                                 excluded_pairs,
+                                                                                                 10)
+test_users_combined = torch.cat([test_user, test_negative_users]).to(device)
+test_items_combined = torch.cat([test_item, test_negative_items]).to(device)
+test_ratings_combined = torch.cat([test_rating, test_negative_ratings]).to(device)
 
 epochs = 50
 for epoch in range(epochs):
     model.train()
-    # 生成训练集负样本
-    train_negative_users, train_negative_items, train_negative_ratings = negative_sampling(
-        data.num_items,
-        train_user.unique(),
-        train_excluded_pairs,
-        num_negatives=10,
-        device=device
-    )
-    train_excluded_pairs.update(zip(train_negative_users.cpu().numpy(), train_negative_items.cpu().numpy()))
-    # 合并正样本和负样本
-    users_combined = torch.cat([train_user, train_negative_users]).to(device)
-    items_combined = torch.cat([train_item, train_negative_items]).to(device)
-    ratings_combined = torch.cat([train_rating, train_negative_ratings]).to(device)
-
     optimizer.zero_grad()
-    predictions = model(users_combined, items_combined)
-    loss = loss_fn(predictions, ratings_combined)
-    loss.backward()
+    predictions_train = model(train_users_combined, train_items_combined)
+    train_loss = loss_fn(predictions_train, train_ratings_combined)
+    train_loss.backward()
     optimizer.step()
 
     model.eval()
-    # 生成测试集负样本，对整个测试集进行评估
-    test_negative_users, test_negative_items, test_negative_ratings = negative_sampling(
-        data.num_items,
-        test_user.unique(),
-        test_excluded_pairs,
-        num_negatives=10,
-        device=device
-    )
-    users_combined_test = torch.cat([test_user, test_negative_users]).to(device)
-    items_combined_test = torch.cat([test_item, test_negative_items]).to(device)
-    ratings_combined_test = torch.cat([test_rating, test_negative_ratings]).to(device)
-
     with torch.no_grad():
-        predictions_test = model(users_combined_test, items_combined_test)
-        test_loss = loss_fn(predictions_test, ratings_combined_test)
-        print(f'Epoch {epoch + 1}: Training loss: {loss.item()} Test Loss: {test_loss.item()}')
+        predictions_validation = model(validation_users_combined, validation_items_combined)
+        validation_loss = loss_fn(predictions_validation, validation_ratings_combined)
+        if epoch < epochs - 1:
+            print(f'Epoch {epoch + 1}: Training loss: {train_loss.item()} Validation Loss: {validation_loss.item()}')
+        else:
+            predictions_test = model(test_users_combined, test_items_combined)
+            test_loss = loss_fn(predictions_test, test_ratings_combined)
+            print(f'Epoch {epoch + 1}: Training loss: {train_loss.item()} Validation Loss: {validation_loss.item()} Test'
+                  f'Loss: {test_loss.item()}')
+
+# 推荐部分
+recommendation = model.recommendation(data.num_users, data.num_items, 100)  # 得到推荐列表矩阵，每一个行代表给每个用户推荐的物品id
+same = 0.0
+p = 0.0
+r = 0.0
+for i in range(data.num_users):
+    real_list = data.data[data.data['user_id'] == i]['item_id'].values  # 用户真实评过分的物品id列表
+    recommendation_list = recommendation[i]  # 用户的推荐列表
+    same += len(set(real_list) & set(recommendation_list))
+    p += len(recommendation_list)  # 推荐总数
+    r += len(real_list)  # 用户真实评过分的物品数目
+precision = same / p
+recall = same / r
+f1 = 2*precision*recall/(precision+recall)
+print(f'Precision: {precision},Recall: {recall},F1: {f1}')
