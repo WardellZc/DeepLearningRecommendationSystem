89eefb7ea47ac74bf7321c11e8f0351fe7a2b27a
WardellZc <164826869+WardellZc@users.noreply.github.com>
2024-04-20 08:43:57 +0800

    Update sampler.py

--- sampler/sampler.py
+++ sampler/sampler.py
@@ -4,18 +4,44 @@
 # @File    : sampler.py
 import random
 
+import pandas as pd
+import torch.nn
+
 
 class Sampler:
     def __init__(self):
         self.negative_users = []
         self.negative_items = []
 
-    def rns(self, num_user: int, num_item: int, excluded_pairs: set, num_negatives: int):
-        for n_user in range(1, num_user + 1):
+    def negative_sampling(self, num_user: int,  # 数据集存在的用户数量
+                          num_item: int,  # 数据集存在的物品数量
+                          excluded_pairs: set,  # 数据集存在的用户-物品对
+                          num_negatives: int):  # 每个用户取多少个负样本
+        for n_user in range(num_user):
             for _ in range(num_negatives):
-                n_item = random.randint(1, num_item)
+                n_item = random.randint(0, num_item - 1)
                 while (n_user, n_item) in excluded_pairs:
-                    n_item = random.randint(1, num_item)
+                    n_item = random.randint(0, num_item - 1)
                 self.negative_users.append(n_user)
                 self.negative_items.append(n_item)
-        return self.negative_users, self.negative_items
+        return torch.tensor(self.negative_users), torch.tensor(self.negative_items), torch.zeros(
+            len(self.negative_users))
+
+    def negative_sampling2(self, num_user: int,  # 数据集存在的用户数量
+                           num_item: int,  # 数据集存在的物品数量
+                           excluded_pairs: set,  # 数据集存在的用户-物品对
+                           num_negatives: int):  # 每个用户取多少个负样本
+        for n_user in range(num_user):
+            for _ in range(num_negatives):
+                n_item = random.randint(0, num_item - 1)
+                while (n_user, n_item) in excluded_pairs:
+                    n_item = random.randint(0, num_item - 1)
+                self.negative_users.append(n_user)
+                self.negative_items.append(n_item)
+        negative_ratings = [0 for _ in range(len(self.negative_users))]
+        df = pd.DataFrame({
+            'user_id': self.negative_users,
+            'item_id': self.negative_items,
+            'rating': negative_ratings
+        })
+        return df
