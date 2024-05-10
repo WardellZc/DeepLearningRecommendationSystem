4125743166848dd2d38fa0b0cb4edc6a72eed474
WardellZc <164826869+WardellZc@users.noreply.github.com>
2024-05-10 15:17:56 +0800

    Update lr.py

--- model/lr.py
+++ model/lr.py
@@ -10,30 +10,11 @@ from torch.nn.init import xavier_normal_
 class LogisticRegression(nn.Module):
     def __init__(self, num_feature: int):
         super().__init__()
-        # 年龄、性别、职业和电影风格的embedding
-        self.age_embeddings = nn.Embedding(1, num_feature)
-        self.gender_embeddings = nn.Embedding(2, num_feature)
-        self.occupations_embeddings = nn.Embedding(21, num_feature)
-        self.movies_embeddings = nn.Embedding(19, num_feature)
-
-        # embedding初始化
-        xavier_normal_(self.age_embeddings.weight.data)
-        xavier_normal_(self.gender_embeddings.weight.data)
-        xavier_normal_(self.occupations_embeddings.weight.data)
-        xavier_normal_(self.movies_embeddings.weight.data)
-
         # 特征向量参数w和b
-        self.linear = nn.Linear(4 * num_feature, 1, True)
+        self.linear = nn.Linear(num_feature, 1, True)
 
     def forward(self, feature_vector: torch.Tensor) -> torch.Tensor:
-        # age:0,gender:1-2;occupation:3-23,movie:24-42
-        age_vector = torch.matmul(feature_vector[:, 0].unsqueeze(1), self.age_embeddings.weight)
-        gender_vector = torch.matmul(feature_vector[:, 1:3], self.gender_embeddings.weight)
-        occupation_vector = torch.matmul(feature_vector[:, 3:24], self.occupations_embeddings.weight)
-        # movie_vector为muti-hot向量,用矩阵乘法加和的方式
-        movie_vector = torch.matmul(feature_vector[:, 24:43], self.movies_embeddings.weight)
-        vector_combined = torch.cat((age_vector, gender_vector, occupation_vector, movie_vector), dim=1)
-        return torch.sigmoid(self.linear(vector_combined))
+        return torch.sigmoid(self.linear(feature_vector))
 
     def recommendation(self, num_users, user_item, k):
         array = []
