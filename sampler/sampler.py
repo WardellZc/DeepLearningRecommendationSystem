62f07927eab65454d10c0c5f16f9b18c835fbecc
WardellZc <164826869+WardellZc@users.noreply.github.com>
2024-05-17 14:08:05 +0800

    Update sampler.py

--- sampler/sampler.py
+++ sampler/sampler.py
@@ -16,7 +16,8 @@ class Sampler:
     def negative_sampling(self, num_user: int,  # 数据集存在的用户数量
                           num_item: int,  # 数据集存在的物品数量
                           excluded_pairs: set,  # 数据集存在的用户-物品对
-                          num_negatives: int):  # 每个用户取多少个负样本
+                          num_negatives: int,  # 每个用户取多少个负样本
+                          device: str = 'cpu'):  # 选择在cpu还是gup上运行
         for n_user in range(num_user):
             for _ in range(num_negatives):
                 n_item = random.randint(0, num_item - 1)
@@ -24,8 +25,8 @@ class Sampler:
                     n_item = random.randint(0, num_item - 1)
                 self.negative_users.append(n_user)
                 self.negative_items.append(n_item)
-        return torch.tensor(self.negative_users), torch.tensor(self.negative_items), torch.zeros(
-            len(self.negative_users))
+        return torch.tensor(self.negative_users).to(device), torch.tensor(self.negative_items).to(device), torch.zeros(
+            len(self.negative_users)).to(device)
 
     def negative_sampling2(self, num_user: int,  # 数据集存在的用户数量
                            num_item: int,  # 数据集存在的物品数量
