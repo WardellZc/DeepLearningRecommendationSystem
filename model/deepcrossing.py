852036600980c3a0ac771e7ad06a0fb38a877e7c
WardellZc <164826869+WardellZc@users.noreply.github.com>
2024-05-17 14:05:46 +0800

    Update deepcrossing.py

--- model/deepcrossing.py
+++ model/deepcrossing.py
@@ -10,6 +10,7 @@ class ResidualBlock(nn.Module):
 
     注意：残差块的输入输出需要一致
     """
+
     def __init__(self, hidden_unit, dim_stack):
         super().__init__()
         # 两个线性层   注意维度， 输出的时候和输入的那个维度一致， 这样才能保证后面的相加
@@ -26,23 +27,27 @@ class ResidualBlock(nn.Module):
 
 
 class DeepCrossing(nn.Module):
-    def __init__(self, num_feature, hidden_units):
+    def __init__(self, num_user, num_item, num_feature, hidden_units):
         """
         num_feature:embedding层的维度
         hidden_units:残差层的维度，也可以用于设置残差层的数量
         """
         super().__init__()
-        # 对类别特征的one-hot向量进行embedding层的转化，gender、occupation、movie
-        self.gender = nn.Embedding(2, num_feature)
-        self.occupation = nn.Embedding(21, num_feature)
-        self.movie = nn.Embedding(19, num_feature)
+        # 对类别特征的one-hot向量进行embedding层的转化，user_id、item_id、gender、occupation、movie
+        self.user_embedding = nn.Embedding(num_user, num_feature)
+        self.item_embedding = nn.Embedding(num_item, num_feature)
+        self.gender_embedding = nn.Embedding(2, num_feature)
+        self.occupation_embedding = nn.Embedding(21, num_feature)
+        self.movie_embedding = nn.Embedding(19, num_feature)
         # 初始化
-        xavier_normal_(self.gender.weight.data)
-        xavier_normal_(self.occupation.weight.data)
-        xavier_normal_(self.movie.weight.data)
+        xavier_normal_(self.user_embedding.weight.data)
+        xavier_normal_(self.item_embedding.weight.data)
+        xavier_normal_(self.gender_embedding.weight.data)
+        xavier_normal_(self.occupation_embedding.weight.data)
+        xavier_normal_(self.movie_embedding.weight.data)
 
         # 统计拼接后的特征维数,类别特征和数值特征的维度和
-        dim_stack = num_feature * 3 + 1
+        dim_stack = num_feature * 5 + 1
 
         # 残差层
         self.res_layers = nn.ModuleList([
@@ -54,13 +59,15 @@ class DeepCrossing(nn.Module):
 
     def forward(self, feature_vector: torch.Tensor) -> torch.Tensor:
         # embedding层
-        gender_embed = torch.matmul(feature_vector[:, 1:3], self.gender.weight)
-        occupation_embed = torch.matmul(feature_vector[:, 3:24], self.occupation.weight)
-        movie_embed = torch.matmul(feature_vector[:, 24:43], self.movie.weight)
-        age = feature_vector[:, 0].unsqueeze(1)
+        user_embed = self.user_embedding(feature_vector[:, 0].long())  # embedding要求是整数类型
+        item_embed = self.item_embedding(feature_vector[:, 1].long())  # embedding要求是整数类型
+        age = feature_vector[:, 2].unsqueeze(1)
+        gender_embed = torch.matmul(feature_vector[:, 3:5], self.gender_embedding.weight)
+        occupation_embed = torch.matmul(feature_vector[:, 5:26], self.occupation_embedding.weight)
+        movie_embed = torch.matmul(feature_vector[:, 26:45], self.movie_embedding.weight)
 
         # stacking层
-        r = torch.cat([age, gender_embed, occupation_embed, movie_embed], dim=1)
+        r = torch.cat([user_embed, item_embed, age, gender_embed, occupation_embed, movie_embed], dim=1)
 
         # Multiple Residual Units层
         for res in self.res_layers:
@@ -72,15 +79,12 @@ class DeepCrossing(nn.Module):
 
     def recommendation(self, num_users, user_item, k):
         array = []
+        device = next(self.parameters()).device
         for i in range(num_users):
             user_vector = user_item[user_item['user_id'] == i]
-            user_vector = torch.Tensor(user_vector.iloc[:, 2:].values)
+            user_vector = torch.Tensor(user_vector.values).to(device)
             scores = self.forward(user_vector)
             values, indices = torch.topk(scores, k, dim=0)
             indices = indices.view(1, -1).tolist()[0]
             array.append(indices)
         return array
-
-
-
-
